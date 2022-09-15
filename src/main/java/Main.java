1fb842886d6f824e40225ca48b193727f8975993
Masha Sechina <mashka_3@mail.ru>
2022-09-15 11:34:40 +0300

    homework-4.1

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long expected = 30;
+        long actual = service.calculate(1000, true);
+        System.out.println(expected + " == ? == " + actual);
+    }
+
+}
